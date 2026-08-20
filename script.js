0a46963e571b9347a0219282f4a9d512fc1377da
tsilvalisbon <tsilvalisbon@gmail.com>
2026-08-20 13:44:26 +0100

    Finished basic math functions

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(n1, n2)
+{
+    let result;
+
+    result = n1 + n2;
+    return (result);
+}
+
+function subtract(n1, n2)
+{
+    let result;
+
+    result = n1 - n2;
+    return (result);
+}
+
+function multiply(n1, n2)
+{
+    let result;
+
+    result = n1 * n2;
+    return (result);
+}
+
+function divide(n1, n2)
+{
+    let result;
+
+    result = n1 / n2;
+    return (result);
+}
