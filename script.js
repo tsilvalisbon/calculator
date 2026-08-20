aa1fc4e7091f9b1752cd0957f4a3b10b7560ae08
tsilvalisbon <tsilvalisbon@gmail.com>
2026-08-20 14:14:16 +0100

    Finished operate function

--- script.js
+++ script.js
@@ -29,3 +29,18 @@ function divide(n1, n2)
     result = n1 / n2;
     return (result);
 }
+
+function operate (n1, n2, operator)
+{
+    let result;
+
+    if (operator === "+")
+        result = add(n1, n2);
+    else if (operator === "-")
+        result = subtract(n1, n2);
+    else if (operator === "*")
+        result = multiply(n1, n2);
+    else if (operator === "/")
+        result = divide(n1, n2);
+    return (result);
+}
